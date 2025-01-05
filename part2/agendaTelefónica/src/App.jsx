712056d979e19c4bf0a75f0bed3c18a9087d57c1
Alvaro Tapia <tapia.alvaro.02@gmail.com>
2025-01-05 01:40:45 -0300

    Adding exercise 2.11

--- part2/agendaTelefónica/src/App.jsx
+++ part2/agendaTelefónica/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import FilterPersons from './components/FilterPersons'
+import axios from 'axios'
 
 const App = () => {
-
+/*
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+*/
 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
@@ -32,19 +35,21 @@ const App = () => {
     persons.find(person => person.name === newName) ? alert(`${newName} is already added to phonebook`) : addPerson()
   }
 
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
-
   const personsToShow = persons.filter(person =>
     person.name.toLowerCase().includes(newFilter.toLowerCase())
   )
+
+  const hook = () => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        setPersons(response.data);        
+      })
+  }
+
+  useEffect(hook, []);
 
   return (
     <div>
