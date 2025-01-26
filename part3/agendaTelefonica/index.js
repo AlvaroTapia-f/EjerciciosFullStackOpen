16bc12624dc828cdfe6282c3c4846f1abaf89c12
Alvaro Tapia <tapia.alvaro.02@gmail.com>
2025-01-25 21:47:46 -0300

    Adding exercise 3.9-3.10

--- part3/agendaTelefonica/index.js
+++ part3/agendaTelefonica/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 
 const morgan = require('morgan')
 
@@ -88,7 +89,7 @@ app.post('/api/persons', (request, response) =>{
     response.json(person)
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
 })
